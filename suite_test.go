3d8cacde123b4a47d7bcd9845398e525d068d5ad
Gorka Guridi <gorka.guridi@affectv.com>
2020-01-09 18:03:07 +0000

    Added test reports for circleci.

--- suite_test.go
+++ suite_test.go
@@ -2,11 +2,15 @@ package models
 
 import (
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 	"testing"
 )
 
 func TestHandlers(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "SameSite cookie support suite")
+	junitReporter := reporters.NewJUnitReporter("test-reports/junit.xml")
+	RunSpecsWithDefaultAndCustomReporters(t, "SameSite cookie support suite", []Reporter{
+		junitReporter,
+	})
 }
